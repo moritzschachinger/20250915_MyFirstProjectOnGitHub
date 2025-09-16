1f9f3dea117eb0ad8764f351128a6816aa836fc8
Moritz Schachinger <M.Schachinger@htlvb.at>
2025-09-16 12:22:25 +0200

    add_something2u3

--- 20250915_MyFirstProjectOnGitHub/Program.cs
+++ 20250915_MyFirstProjectOnGitHub/Program.cs
@@ -2,3 +2,5 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("add_something1");
+Console.WriteLine("add_something2");
+Console.WriteLine("add_something3");
